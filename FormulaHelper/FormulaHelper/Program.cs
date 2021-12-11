4b134f6bae911dfd164ba56d65ce78d0e1b67968
Popkov Ilya <popkov.ilya.alexandrovich@gmail.com>
2021-12-11 17:03:12 +0300

    Basis
    1) Добавлено привеltние к базисы AND-OR
    2) Добавлена функция замены знаков AND побелами
    3) Исправлена проблема со скобками
    4) Исправлена проблема со слиянием прилежащих знаков отрицания

--- FormulaHelper/FormulaHelper/Program.cs
+++ FormulaHelper/FormulaHelper/Program.cs
@@ -7,9 +7,39 @@ namespace FormulaHelper
 {
     class Program
     {
-        public static string RegexReplace(string text)
+        
+
+        public static string RM_AND(string raw_formula) => Regex.Replace(raw_formula, @"[\s]*∧[\s]*", @" \; "); // todo: добавить рассмотрение скобок
+
+        public static string Basis(string raw_formula)
         {
-            string output = text;
+            string[] components = raw_formula.Split("∨");
+            for (int i = 0; i < components.Length; i++) components[i] = "¬(" + components[i] + ")";
+            return "¬(" + String.Join("∧", components) + ")";
+        }
+
+        public static string Over_MA(Match m)
+        {
+            Console.WriteLine($"#{m.Value}#");
+            string output = m.Value;
+            if (output != "")
+            {
+                output = output.TrimStart('¬');
+                if (output.StartsWith("(") && output.EndsWith(")")) output = output.Substring(1, output.Length - 2);
+                return @" \overline{" + output + @"} ";
+            }
+            else
+            {
+                Console.WriteLine($"${output}$");
+                return "";
+            }
+        }
+
+        public static string OVER_MA(Match m) => (m.Value.StartsWith("¬(") && m.Value.EndsWith(")")) ? m.Value.Substring(2, m.Value.Length - 3) : m.Value.TrimStart('¬');
+
+        public static string LatexConverter(string raw_formula)
+        {
+            string output = raw_formula;
             var symbols = new Dictionary<string, string>()
             {
                 ["∧"] = @"\land",
@@ -17,22 +47,31 @@ namespace FormulaHelper
                 ["∩"] = @"\cap",
                 ["∪"] = @"\cup"
             };
-            // отрицательные значения также предусмотрены
-            output = Regex.Replace(output, @"[\s]*", ""); // убрать все пробелы из исходной строки
             output = Regex.Replace(output, @"X[^\d]", "X1"); // X → X1 (X0)
-            output = Regex.Replace(output, @"(¬\(.*\))|(¬[\-\^\w]*)", m => @"\overline{" + Regex.Replace(m.Value, @"¬|\(|\)", "") + @"}"); // Замена отрицания
-            output = Regex.Replace(output, @"(?<=X[\-_\^\d]*\}?\)?)[^\d](?=[\-\(]*((X)|(\\over)))", m => @$" {symbols.GetValueOrDefault(m.Value, m.Value)} "); // Замена операндов
-            output = Regex.Replace(output, @"X[\d]*", m => @$"X_{{{Convert.ToInt32(m.Value.Substring(m.Value.IndexOf('X') + 1)) - 1}}}"); // Уменьшение индексов
+            while (output.Contains("¬"))
+            {
+                output = Regex.Replace(output, @"(¬\([^\(\)]*\))|(¬\-?X[\d]*)", Over_MA); // только для одиночных иксов или скобок
+            }
+            output = Regex.Replace(output, @"(?<=X[\-_\^\d]*[\s\}\\\)]*)[^\w\s\d\\{};](?=[\-\\\(\s]*((X)|(\\over)))", m => @$" {symbols.GetValueOrDefault(m.Value, m.Value)} "); // Замена операндов
+            output = Regex.Replace(output, @"X[\d]*", m => @$"X_{{{Convert.ToInt32(m.Value.Substring(m.Value.IndexOf('X') + 1)) - 1}}} "); // Уменьшение индексов
             output = Regex.Replace(output, @"\^[\-\d]*", m => @$"^{{{m.Value.TrimStart('^')}}}"); // Степени
             output = Regex.Replace(output, @"\^\{[\d]\}*(?=[^\}\s])", m => @$"{m.Value} "); // todo: Костыль со степенями и пробелами
+            output = Regex.Replace(output, @"[\s]+", " ");
             return output;
         }
 
         static void Main(string[] args)
         {
-            string rawFormule = "¬-X12^2∧-X2∨X3∧-¬X4∧¬(X5^-5∧-X10)∨X1^2¬X2^3∧X2∧X3";
-            Console.WriteLine(rawFormule + "\n");
-            Console.WriteLine(RegexReplace(rawFormule) + "\n");
+            string rawFormule = "¬X1∧¬X2∧¬X3∧¬X4∧¬X5∨X1∧X2∧X3∧¬X4∧¬X5∨X1∧¬X2∧X3∧¬X4∧¬X5∨¬X1∧¬X2∧X3∧¬X4∧¬X5∨¬X1∧X2∧¬X3∧X4∧¬X5∨¬X1∧X2∧X3∧X4∧¬X5∨¬X1∧¬X2∧¬X3∧¬X4∧X5∨¬X1∧X2∧¬X3∧¬X4∧X5";
+            Console.WriteLine("raw\t" + rawFormule + "\n");
+
+            string output = RM_AND(rawFormule);
+            Console.WriteLine("step1\t" + output + "\n");
+            output = Basis(output);
+            Console.WriteLine("step2\t" + output + "\n");
+            output = LatexConverter(output);
+
+            Console.WriteLine("final\t" + output + "\n");
             Console.ReadLine();
         }
     }
